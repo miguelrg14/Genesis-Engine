e75d4d34d10175cd7a2d6ae0782e2c9c9f8995f0
Miguel Rodríguez Gallego <miguelrodriguezgallego1@gmail.com>
2026-02-26 00:48:35 +0100

    Project Setup and Entry Point.

---
+++ Genesis/src/Genesis/Core.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#ifdef GS_PLATFORM_WINDOWS
+	#ifdef GS_BUILD_DLL
+		#define GENESIS_API __declspec(dllexport)
+	#else
+		#define GENESIS_API __declspec(dllimport)
+	#endif
+
+#else
+	#error Genesis only supports Windows!
+
+#endif
