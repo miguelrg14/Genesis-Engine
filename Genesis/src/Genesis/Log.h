17efb3eaae612d4a6532b8623e215855ad42b75c
Miguel Rodríguez Gallego <miguelrodriguezgallego1@gmail.com>
2026-03-09 19:07:25 +0100

    Loggin implementation (spdlog library inclusion through submodule technology)

---
+++ Genesis/src/Genesis/Log.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <memory>
+
+#include "Core.h"
+#include "spdlog/spdlog.h"
+
+namespace Genesis {
+
+	class GENESIS_API Log
+	{
+	public:
+		static void Init();
+
+		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+
+	private:
+		static std::shared_ptr<spdlog::logger> s_CoreLogger;
+		static std::shared_ptr<spdlog::logger> s_ClientLogger;
+	};
+
+}
+
+// Core log macros
+#define GS_CORE_TRACE(...)    ::Genesis::Log::GetCoreLogger()   ->    trace(__VA_ARGS__);
+#define GS_CORE_DEBUG(...)    ::Genesis::Log::GetCoreLogger()   ->    debug(__VA_ARGS__);
+#define GS_CORE_INFO(...)     ::Genesis::Log::GetCoreLogger()   ->     info(__VA_ARGS__);
+#define GS_CORE_WARN(...)     ::Genesis::Log::GetCoreLogger()   ->     warn(__VA_ARGS__);
+#define GS_CORE_ERROR(...)    ::Genesis::Log::GetCoreLogger()   ->    error(__VA_ARGS__);
+#define GS_CORE_CRITICAL(...) ::Genesis::Log::GetCoreLogger()   -> critical(__VA_ARGS__);
+
+// Client log macros
+#define GS_TRACE(...)         ::Genesis::Log::GetClientLogger() ->    trace(__VA_ARGS__);
+#define GS_DEBUG(...)         ::Genesis::Log::GetCoreLogger()   ->    debug(__VA_ARGS__);
+#define GS_INFO(...)          ::Genesis::Log::GetClientLogger() ->     info(__VA_ARGS__);
+#define GS_WARN(...)          ::Genesis::Log::GetClientLogger() ->     warn(__VA_ARGS__);
+#define GS_ERROR(...)         ::Genesis::Log::GetClientLogger() ->    error(__VA_ARGS__);
+#define GS_CRITICAL(...)      ::Genesis::Log::GetClientLogger() -> critical(__VA_ARGS__);
+
