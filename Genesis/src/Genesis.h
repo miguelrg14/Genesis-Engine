bec4498579b321d232507cdbad0e64865dfc4644
Miguel Rodríguez Gallego <miguelrodriguezgallego1@gmail.com>
2026-03-09 19:08:20 +0100

    Log implementation code and test.

--- Genesis/src/Genesis.h
+++ Genesis/src/Genesis.h
@@ -3,6 +3,7 @@
 // For use by Genesis applications
 
 #include "Genesis/Application.h"
+#include "Genesis/Log.h"
 
 // --- Entry Point ---
 #include "Genesis/EntryPoint.h"
--- Genesis/src/Genesis/EntryPoint.h
+++ Genesis/src/Genesis/EntryPoint.h
@@ -6,6 +6,18 @@ extern Genesis::Application* Genesis::CreateApplication();
 
 int main(int argc, char** argv)
 {
+	Genesis::Log::Init();
+	GS_CORE_WARN("Initialized Log!");
+	int a = 5;
+	GS_CORE_INFO("Hello! Var={0}", a);
+
+	GS_CORE_TRACE("Trace");
+	GS_CORE_DEBUG("Debug");
+	GS_CORE_INFO("Info");
+	GS_CORE_WARN("Warn");
+	GS_CORE_ERROR("Error!");
+	GS_CORE_CRITICAL("Critical!");
+
 	auto app = Genesis::CreateApplication();
 	app->Run();
 	delete app;
